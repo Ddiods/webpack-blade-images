fe3f89ff792128a0a96d966fb5c4ba7636771fb8
david.soto <david.soto@cignium.com>
2016-09-02 17:24:41 -0500

    - Small Fix

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,35 @@
-function WebPackBladeImages(options) {
-}
+'use strict';
 
-WebPackBladeImages.prototype.apply = function (compiler) {
-  compiler.plugin('emit', function (compilation, callback) {
-    // Create a header string for the generated file:
-    var filelist = 'In this build:\n\n';
+function WebPackBladeImages() {
+  var apply = function apply(compiler) {
+    compiler.plugin('emit', function (compilation, callback) {
+      // Create a header string for the generated file:
+      var filelist = 'In this build:\n\n';
 
-    // Loop through all compiled assets,
-    // adding a new line item for each filename.
-    for (var filename in compilation.assets) {
-      filelist += ('- ' + filename + '\n');
-    }
-
-    // Insert this list into the Webpack build as a new file asset:
-    compilation.assets['filelist.md'] = {
-      source: function () {
-        return filelist;
-      },
-      size: function () {
-        return filelist.length;
+      // Loop through all compiled assets,
+      // adding a new line item for each filename.
+      for (var filename in compilation.assets) {
+        filelist += ('- ' + filename + '\n');
       }
-    };
 
-    callback();
-  });
-};
+      // Insert this list into the Webpack build as a new file asset:
+      compilation.assets['filelist.md'] = {
+        source: function () {
+          return filelist;
+        },
+        size: function () {
+          return filelist.length;
+        }
+      };
 
+      callback();
+    });
+
+  };
+
+  return {
+    apply: apply
+  }
+}
+WebPackBladeImages['default'] = WebPackBladeImages;
 module.exports = WebPackBladeImages;
